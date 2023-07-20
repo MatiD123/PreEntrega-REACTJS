730a6dcbec93b5d3fad6f6a86cdad46f1702eb93
Matias Diano <matidiano4@gmail.com>
2023-07-20 12:10:04 -0300

    first commit

---
+++ Reactjss/src/components/Navbar/index.jsx
@@ -0,0 +1,17 @@
+function Navbar() {
+    return (
+        <header>
+            <div className="container-nav">
+                <h1 className="titulo">Bernal Wood</h1>
+                <nav>
+                    <ul>
+                        <li>Catalogo</li>
+                        <li>Contacto</li>
+                    </ul>
+
+                </nav>
+            </div>
+        </header>
+    )
+}
+export default Navbar
